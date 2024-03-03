f2f9ea30e55a44da5ad77b0995047c1b2f439d77
@andatoshiki <hello@toshiki.dev>
2024-03-03 03:22:53 -0700

    fix: specify remote repository linkto gitlab by using `call` function method to fetch remote origin dependency sources

--- external/r8125/r8125.mk
+++ external/r8125/r8125.mk
@@ -1,5 +1,5 @@
 # R8125_VERSION = 99cd3bc868e4ba82a473d8efaedad04fedb0c0f7
-R8125_SITE = https://gitlab.com/toshikidev/shikiology/shiki-v1/shiki-r8125
+R8125_SITE = $(call gitlab,toshikidev,shikiology,shiki-v1,shiki-r8125)
 R8125_LICENSE = GPL-2.0
 
 $(eval $(kernel-module))
--- external/r8168/r8168.mk
+++ external/r8168/r8168.mk
@@ -1,5 +1,5 @@
 # R8168_VERSION = 52c98bd764e6dd22ff17876afa655e9e11237cc9
-R8168_SITE = https://gitlab.com/toshikidev/shikiology/shiki-v1/shiki-r8168
+R8168_SITE = R8125_SITE = $(call gitlab,toshikidev,shikiology,shiki-v1,shiki-r8168)
 R8168_LICENSE = GPL-2.0
 
 $(eval $(kernel-module))
