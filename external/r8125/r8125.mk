059dd84e6402fb09f44fcbcb325b820ce19ce43f
@andatoshiki <hello@toshiki.dev>
2024-03-03 08:48:04 -0700

    chore: add instructions and comment for obtaining package version by git logging commit hash as rundown

--- external/r8125/r8125.mk
+++ external/r8125/r8125.mk
@@ -1,3 +1,5 @@
+# git commit hash of specific commit identifier
+# rundown: git rev-parse --verify HEAD
 R8125_VERSION = b59380e5c27c9671f44c1e2968ad115ffca200bd
 R8125_SITE = $(call github,shikiology,r8125,$(R8125_VERSION))
 R8125_LICENSE = GPL-2.0
--- external/r8168/r8168.mk
+++ external/r8168/r8168.mk
@@ -1,3 +1,5 @@
+# git commit hash of specific commit identifier
+# rundown: git rev-parse --verify HEAD
 R8168_VERSION = 0e477c861eabae29cb91cc0bda68913ae4778797
 R8168_SITE = R8125_SITE = $(call github,shikiology,r8168,$(R8168_VERSION))
 R8168_LICENSE = GPL-2.0
